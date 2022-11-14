bcc8a6615d31da6da930cf70baebe3121c7d51f5
Ambarish Bhagawati <ambarish8653@gmail.com>
2022-11-14 19:12:28 +0530

    tests added

--- Practice/src/Hello/Practive.java
+++ Practice/src/Hello/Practive.java
@@ -3,9 +3,14 @@ package Hello;
 public class Practive {
 	
 	public static void main(String[] args) {
-		char[] name="india".toCharArray();
-		for(int i=0;i<name.length;i++) {
-			if(name[i] )
-		}
+		char c='A';
+		c+=3;
+		System.out.println(c);
+		char c1='O';
+		c1+=32;
+		System.out.println(c1);
+		char c2='d';
+		c2-=32;
+		System.out.println(c2);
 	}
 }
