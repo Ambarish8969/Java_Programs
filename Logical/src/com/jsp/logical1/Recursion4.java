621ea4960a0fcae1612eaa5abb05ce8715e8d3d0
Ambarish Bhagawati <ambarish8653@gmail.com>
2022-12-16 22:51:21 +0530

    test files added

--- Logical/src/com/jsp/logical1/Recursion4.java
+++ Logical/src/com/jsp/logical1/Recursion4.java
@@ -1,12 +1,14 @@
 package com.jsp.logical1;
 
+// Factorial of a Number using recursion.
+
 public class Recursion4 {
 	public static int fact(int n) {
 		if(n==1) {
 			return 1;
 		}else { 
 			return n*fact(n-1);
-		}
+		} 
 	}
 	public static void main(String[] args) {
 		System.out.println(fact(6));
