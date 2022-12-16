dd277ca54ff0e09ad1a7e5c2d14f072f177df891
Ambarish Bhagawati <ambarish8653@gmail.com>
2022-12-16 22:05:22 +0530

    test files added

---
+++ Logical/src/com/jsp/logical1/NthPrime.java
@@ -0,0 +1,31 @@
+package com.jsp.logical1;
+
+public class NthPrime {
+	static boolean isPrime(int n) {
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                count++;
+            }
+        } 
+        if(count==2){
+        	return true;
+        }else{
+        	return false;
+        }
+	}
+	public static void main(String[] args) {
+		int n1=1,c=0;
+		int n=4;
+		while(true) {
+			boolean ans=isPrime(n1);
+			if(ans==true) {
+				c++;
+			}if(c==n) {
+				System.out.println(n1);
+				break;
+			}
+			n1++;
+		}
+	}
+}
