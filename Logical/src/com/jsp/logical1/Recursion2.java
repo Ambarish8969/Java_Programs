c9db893e5136985405c0142c85601e8c5cdde6ac
Ambarish Bhagawati <ambarish8653@gmail.com>
2022-12-16 22:36:13 +0530

    test files added

---
+++ Logical/src/com/jsp/logical1/Recursion2.java
@@ -0,0 +1,17 @@
+package com.jsp.logical1;
+
+// Print 1 to 10 in reverse order without using loops.
+
+public class Recursion2 {
+	public static void demo(int n) {
+		if(n>=1) {
+			System.out.println(n);
+			n--;
+			demo(n);
+		}
+	}
+	public static void main(String[] args) {
+		demo(10);
+	}
+
+}
---
+++ Logical/src/com/jsp/logical1/Recursion3.java
@@ -0,0 +1,16 @@
+package com.jsp.logical1;
+
+// Print sum of n numbers using recursion.
+public class Recursion3 {
+	public static int demo(int n) {
+		if(n==0) {
+			return 0;
+		}else {
+			return n+demo(n-1);
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println(demo(10));
+	}
+
+}
