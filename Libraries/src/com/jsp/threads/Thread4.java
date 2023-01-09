e8339baab7621601b7e7b817a6cb0e852791e65b
Ambarish Bhagawati <ambarish8653@gmail.com>
2023-01-09 19:37:15 +0530

    Thread4 added

--- Libraries/src/com/jsp/threads/Thread4.java
+++ Libraries/src/com/jsp/threads/Thread4.java
@@ -1,24 +1,23 @@
-package Hello;
+package com.jsp.threads;
 
-class Sample extends Thread{
-	@Override
+class Sample12 extends Thread{
+	@Override 
 	public void run() {
 		for(int i=0;i<5;i++) {
 			System.out.println(i);
 		}
 	}
 }
-public class Practice1 extends Thread{
+public class Thread4 extends Thread {
 	@Override
 	public void run() {
 		for(int i=4;i>=0;i--) {
 			System.out.println(i);
 		}
 	}
-	
 	public static void main(String[] args) {
-		Sample s1=new Sample();
-		Practice1 p1=new Practice1();
+		Sample12 s1=new Sample12();
+		Thread4 p1=new Thread4();
 		s1.start();
 		//System.out.println("-----------------------------");
 		p1.start();
