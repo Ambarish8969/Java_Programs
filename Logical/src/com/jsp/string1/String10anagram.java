1b79e5e44e3507a8c3f8abc44b02acad887cf663
Ambarish Bhagawati <ambarish8653@gmail.com>
2023-01-12 19:49:48 +0530

    string10anagram file added

---
+++ Logical/src/com/jsp/string1/String10anagram.java
@@ -0,0 +1,24 @@
+package com.jsp.string1;
+
+// Anagram means length and characters of both strings 
+// should be same.
+public class String10anagram {
+	public static void main(String[] args) {
+		String s1="hello";
+		String s2="olehl"; 
+		if(s1.length()==s2.length()) {
+			char arr1[]=s1.toCharArray();
+			char arr2[]=s2.toCharArray();
+			for(int i=0;i<s1.length();i++) {
+				for(int j=0;j<s1.length();j++) {
+					if(arr1[i]==arr2[j]) {
+						arr2[j]='\0';
+					} 
+				}
+			}
+			System.out.println("Anagram");
+		}else {
+			System.out.println("Not an Anagram");
+		}
+	}
+}
